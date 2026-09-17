4db0a9a59387be41835aff85c183caabb3a01e02
NovaPMV <NovaPMV@proton.me>
2026-09-17 01:38:25 -0700

    Delete Start TokTidy.bat

--- Start TokTidy.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem Starts TokTidy. (The desktop shortcut does the same thing without this window.)
-cd /d "%~dp0"
-if not exist "app\node_modules\electron\dist\electron.exe" (
-  echo TokTidy is not installed yet. Double-click setup.bat first.
-  pause
-  exit /b 1
-)
-start "" "app\node_modules\electron\dist\electron.exe" "%~dp0app"
